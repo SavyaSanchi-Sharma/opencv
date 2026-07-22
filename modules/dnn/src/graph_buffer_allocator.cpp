69d53efe06e72b22f224101905fcbcef55577437
Savya Sanchi Sharma <savyasanchisharma.official@gmail.com>
2026-07-22 10:04:42 +0530

    cuDNN JIT + new graph engine: CUDA/UMat backend, engine-classic removal

--- modules/dnn/src/graph_buffer_allocator.cpp
+++ modules/dnn/src/graph_buffer_allocator.cpp
@@ -160,7 +160,7 @@ struct BufferAllocator
         netimpl->bufidxs = bufidxs;
         netimpl->buffers.resize(nbufs);
         for (int i = 0; i < nbufs; i++)
-            netimpl->buffers[i] = Mat();
+            netimpl->buffers[i] = UMat();
     }
 
     void assign(const Ptr<Graph>& graph)
@@ -183,7 +183,7 @@ struct BufferAllocator
                 }
             }
         }
-        const std::vector<Ptr<Layer> >& prog = graph->prog();
+        const std::vector<Ptr<LayerInfo> >& prog = graph->prog();
         for (const auto& layer: prog) {
             bool inplace = false;
             Arg reuseArg;
@@ -362,13 +362,13 @@ struct BufferAllocator
                 std::unordered_set<int> bodyDefined;
                 for (Arg ba : body->inputs())
                     bodyDefined.insert(ba.idx);
-                for (const Ptr<Layer>& blayer : body->prog()) {
+                for (const Ptr<LayerInfo>& blayer : body->prog()) {
                     if (!blayer) continue;
                     for (Arg bo : blayer->outputs)
                         bodyDefined.insert(bo.idx);
                 }
                 std::unordered_set<int> closureBumped;
-                for (const Ptr<Layer>& blayer : body->prog()) {
+                for (const Ptr<LayerInfo>& blayer : body->prog()) {
                     if (!blayer) continue;
                     for (Arg bi : blayer->inputs) {
                         if (bi.idx <= 0) continue;
--- modules/dnn/src/layers/batch_norm2_layer.cpp
+++ modules/dnn/src/layers/batch_norm2_layer.cpp
@@ -7,6 +7,10 @@
 #include "../precomp.hpp"
 #include "layers_common.hpp"
 #include "../net_impl.hpp"
+#include "../op_cuda.hpp"
+#ifdef HAVE_CUDA
+#include "../cuda4dnn/primitives/batch_norm.hpp"
+#endif
 
 namespace cv {
 namespace dnn {
@@ -355,8 +359,36 @@ public:
 
     bool supportBackend(int backendId) CV_OVERRIDE
     {
-        return backendId == DNN_BACKEND_OPENCV;
+        return backendId == DNN_BACKEND_OPENCV
+#ifdef HAVE_CUDA
+               || backendId == DNN_BACKEND_CUDA
+#endif
+               ;
     }
+
+#ifdef HAVE_CUDA
+    // Channel-wise scale+shift on CUDA; reused by the new graph engine via CUDALegacyExec.
+    Ptr<BackendNode> initCUDA(void* context_,
+                              InputArrayOfArrays,
+                              InputArrayOfArrays) CV_OVERRIDE
+    {
+        auto context = reinterpret_cast<cuda4dnn::csl::CSLContext*>(context_);
+        Mat scale_, bias_;
+        if (inputs.size() == 1) {
+            getScaleBias(scale_, bias_);  // already frozen by graph_const_args
+        } else {
+            CV_Assert(inputs.size() == 5);
+            auto netimpl_ = getNetImpl(this);
+            Mat sc = netimpl_->argTensor(inputs[1]).getMat(ACCESS_READ);
+            Mat bs = netimpl_->argTensor(inputs[2]).getMat(ACCESS_READ);
+            Mat mn = netimpl_->argTensor(inputs[3]).getMat(ACCESS_READ);
+            Mat vr = netimpl_->argTensor(inputs[4]).getMat(ACCESS_READ);
+            BatchNorm2Layer::getScaleBias(sc, bs, mn, vr, epsilon, scale_, bias_);
+        }
+        return make_cuda_node<cuda4dnn::BatchNormOp>(
+            preferableTarget, std::move(context->stream), scale_, bias_);
+    }
+#endif
 
     MatShape getOutShape(const MatShape& inpShape) const
     {
@@ -466,10 +498,10 @@ public:
             !netimpl_->isConstArg(inputs[3]) ||
             !netimpl_->isConstArg(inputs[4]))
             return false;
-        Mat scale_ = netimpl_->argTensor(inputs[1]);
-        Mat bias_ = netimpl_->argTensor(inputs[2]);
-        Mat mean_ = netimpl_->argTensor(inputs[3]);
-        Mat var_ = netimpl_->argTensor(inputs[4]);
+        Mat scale_ = netimpl_->argTensor(inputs[1]).getMat(ACCESS_READ);
+        Mat bias_ = netimpl_->argTensor(inputs[2]).getMat(ACCESS_READ);
+        Mat mean_ = netimpl_->argTensor(inputs[3]).getMat(ACCESS_READ);
+        Mat var_ = netimpl_->argTensor(inputs[4]).getMat(ACCESS_READ);
         BatchNorm2Layer::getScaleBias(scale_, bias_, mean_, var_, epsilon, scale, bias);
         inputs.resize(1);
         return true;
